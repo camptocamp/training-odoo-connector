f834b5a8a8ccbd85bf2b6f6c83385cb848ba355c
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2016-09-29 12:11:46 +0200

    Use a job in a synchronous way

--- my_addons/connector_odoo/models/res_partner.py
+++ my_addons/connector_odoo/models/res_partner.py
@@ -2,6 +2,7 @@
 
 import logging
 
+from openerp.addons.connector.queue.job import job
 from openerp.addons.connector.event import on_record_create, on_record_write
 
 _logger = logging.getLogger(__name__)
@@ -10,4 +11,10 @@ _logger = logging.getLogger(__name__)
 @on_record_create(model_names=['res.partner'])
 @on_record_write(model_names=['res.partner'])
 def on_change_export_partner(session, model_name, record_id, vals):
+    export_partner(session, model_name, record_id)
+
+
+@job
+def export_partner(session, model_name, record_id):
+    """ Exporting a partner """
     _logger.info('exporting %s with id %d', model_name, record_id)
